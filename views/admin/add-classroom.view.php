1ae84d1ead6e83cda7e960043cf38c175d11f3a2
blackerby <wmblackerby@gmail.com>
2022-09-02 13:45:10 -0500

    Alter add and edit teacher and classroom markup

--- views/admin/add-classroom.view.php
+++ views/admin/add-classroom.view.php
@@ -6,43 +6,43 @@
     </div>
   <h3>Schedule</h3>
     <div class="mb-3">
-      <div class="form-check">
+      <div class="form-check form-check-inline">
         <input class="form-check-input" type="checkbox" value="1" name="schedule[]" id="a_block">
         <label class="form-check-label" for="a_block">
           A Block
         </label>
       </div>
-      <div class="form-check">
+      <div class="form-check form-check-inline">
         <input class="form-check-input" type="checkbox" value="3" name="schedule[]" id="b_block">
         <label class="form-check-label" for="b_block">
           B Block
         </label>
       </div>
-      <div class="form-check">
+      <div class="form-check form-check-inline">
         <input class="form-check-input" type="checkbox" value="4" name="schedule[]" id="c_block">
         <label class="form-check-label" for="c_block">
           C Block
         </label>
       </div>
-      <div class="form-check">
+      <div class="form-check form-check-inline">
         <input class="form-check-input" type="checkbox" value="5" name="schedule[]" id="d_block">
         <label class="form-check-label" for="d_block">
           D Block
         </label>
       </div>
-      <div class="form-check">
+      <div class="form-check form-check-inline">
         <input class="form-check-input" type="checkbox" value="6" name="schedule[]" id="e_block">
         <label class="form-check-label" for="e_block">
           E Block
         </label>
       </div>
-      <div class="form-check">
+      <div class="form-check form-check-inline">
         <input class="form-check-input" type="checkbox" value="7" name="schedule[]" id="f_block">
         <label class="form-check-label" for="f_block">
           F Block
         </label>
       </div>
-      <div class="form-check">
+      <div class="form-check form-check-inline">
         <input class="form-check-input" type="checkbox" value="8" name="schedule[]" id="g_block">
         <label class="form-check-label" for="g_block">
           G Block
